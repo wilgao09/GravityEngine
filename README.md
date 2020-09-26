7688359c465d768dfc5974d882a789957c7578e3
wilgao09 <william.gao09@gmail.com>
2020-09-26 15:56:59 -0400

    added webserver.js

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # GravityEngine
+
+## Purpose
+In physics classes, instructors talk about the four fundamental forces (Gravity, the Weak Force, Strong Force, and Electromagnetic Force). However, just saying "they're weak" undermines how weak they actually are. This project seeks to demonstrate how much mass and how small of a distance is required in order to have a meaningful interaction between macroscopic bodies. 
+
+## Background
+
+
+## Build tools
+The display is written in HTML5 and CSS. Dynamic resizing and physics calculations are all done in the front end using Javascript.
+
---
+++ webserver.js
@@ -0,0 +1,18 @@
+
+
+var http = require('http');
+var fs = require('fs');
+
+http.createServer((req,res) => {
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Methods","GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers","X-Requested-With, contenttype");
+    res.setHeader("Access-Control-Allow-Credentials",true);
+
+    res.writeHead(200,{"Content-Type":"text/html"});
+    fs.readFile("index.html", (err, dat) => {
+        if (err) res.write("error; try again?");
+        else res.write(dat);
+    });
+    res.end();
+}).listen(80);
